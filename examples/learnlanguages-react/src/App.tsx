93bf720881390f8d428c108c9d09c764ae6ff09c
albertincx <albertincx@gmail.com>
2024-05-18 18:19:21 +0700

    prompt added

--- examples/learnlanguages-react/src/App.tsx
+++ examples/learnlanguages-react/src/App.tsx
@@ -1,17 +1,17 @@
-import 'regenerator-runtime/runtime';
-import React from 'react';
+import 'regenerator-runtime/runtime'
+import React from 'react'
 
-import DefaultLayout from './containers/DefaultLayout/DefaultLayout';
-import ReloadPrompt from './Reload'
-import './main.css';
+import DefaultLayout from './containers/DefaultLayout/DefaultLayout'
+import ReloadPrompt from './ReloadPrompt'
+import './main.css'
 
-const App = () => {
-    return (
-        <>
-            <DefaultLayout />
-            <ReloadPrompt />
-        </>
-    )
+function App() {
+  return (
+    <>
+      <DefaultLayout />
+      <ReloadPrompt />
+    </>
+  )
 }
 
-export default App;
+export default App
